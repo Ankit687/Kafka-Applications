7e7090713650df1b0d3a6839a4e3607c6567d948
Ankit Kumar <Ankit687kumar@gmail.com>
2025-07-05 22:12:43 +0530

    Consumer: enhance message listener to include key and payload in consumption

--- Kafka-Consumer/src/main/java/com/watcix/kafkaconsumer/service/ConsumerService.java
+++ Kafka-Consumer/src/main/java/com/watcix/kafkaconsumer/service/ConsumerService.java
@@ -1,13 +1,16 @@
 package com.watcix.kafkaconsumer.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerService {
 
-    @KafkaListener(topics = "${kafka.topic.name}", groupId = "connection-consumer-group")
-    public void listen(String message){
-        System.out.println("Consumed message: " + message);
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(@Header(KafkaHeaders.RECEIVED_KEY) String key, @Payload String payload) {
+        System.out.println("Consumed message: " + payload + " with key: " + key);
     }
 }
